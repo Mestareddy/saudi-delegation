4b4fcf74bea4e1a03600552f3dedcca95cc42473
Edikan Bassey <edikan.i.bassey@gmail.com>
2024-01-09 09:58:26 +0100

    speakers

---
+++ src/mockData/speakers.ts
@@ -0,0 +1,50 @@
+export const speakers = [
+  {
+    id: 1,
+    name: "Dr. Aisha Al-Farsi",
+    field: "Keynote Speaker - Industry Visionary",
+    experience:
+      "Join us for an inspiring keynote address from Aisha Al-Farsi, an industry visionary renowned for groundbreaking insights and a wealth of experience. Prepare to gain a fresh perspective on the future of our industry.",
+    image: "/images/dr_aisha_al_farsi.png",
+  },
+  {
+    id: 2,
+    name: "Chijioke Nwankwo",
+    field: "Entrepreneurial Journey",
+    experience:
+      "Be inspired by the entrepreneurial journey of Chijioke Nwankwo, a dynamic individual who turned vision into reality. Learn about overcoming challenges, seizing opportunities, and the invaluable lessons learned along the way.",
+    image: "/images/chijioke_nwankwo.png",
+  },
+  {
+    id: 3,
+    name: "Ahmed Abubakar",
+    field: "Tech Innovator",
+    experience:
+      "Dive into the latest technological trends with Ahmed Abubakar, a tech innovator leading the charge in transformative solutions. Learn about cutting-edge advancements and how they're reshaping the landscape.",
+    image: "/images/ahmed_abubakar.png",
+  },
+  {
+    id: 4,
+    name: "Layla Ahmed",
+    field: "Diversity and Inclusion Advocate",
+    experience:
+      "Join Layla Ahmed in exploring the importance of diversity and inclusion in the workplace. Gain insights into fostering a culture that celebrates differences, drives innovation, and ensures a thriving, inclusive environment.",
+    image: "/images/layla_ahmed.png",
+  },
+  {
+    id: 5,
+    name: "Fatima Ibrahim",
+    field: "Customer Success Expert",
+    experience:
+      "Discover the keys to exceptional customer success with Fatima Ibrahim, a seasoned expert in building and maintaining strong customer relationships. Gain practical insights and strategies for elevating your customer satisfaction.",
+    image: "/images/fatima_ibrahim.png",
+  },
+  {
+    id: 6,
+    name: "Olumide Adeleke",
+    field: "Leadership Excellence",
+    experience:
+      "Elevate your leadership skills with insights from Olumide Adeleke, a proven leader in the field. Discover effective strategies for leading high-performing teams, navigating challenges, and fostering a culture of success.",
+    image: "/images/olumide_adeleke.png",
+  },
+];
