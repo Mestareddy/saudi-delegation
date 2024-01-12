33b1a50c7a866605efd93975120be3af9a087268
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2024-01-12 11:59:40 +0100

    chore:mopdified auth style

--- src/components/auth/AuthHeader.tsx
+++ src/components/auth/AuthHeader.tsx
@@ -7,12 +7,12 @@ const AuthHeader: React.FunctionComponent = () => {
       <Paragraph className="font-normal md:basis-2/4">
         In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface is a without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.
       </Paragraph>
-      <div className="flex flex-row items-center space-x-2.5 my-10">
-        <div className="h-[3px] w-[15%] bg-green-hover" />
+      <div className="flex flex-row items-center space-x-2.5 my-2.5 md:my-10">
+        <div className="h-[3px] w-[26%] md:w-[15%] bg-green-hover" />
         <Heading type="h3" className="font-bold">
           Monday, January 29, 2024
         </Heading>
-        <div className="h-[3px] w-[15%] bg-green-hover" />
+        <div className="h-[3px] w-[26%] md:w-[15%] bg-green-hover" />
       </div>
     </div>
   )
--- src/components/auth/RegistrationModal.tsx
+++ src/components/auth/RegistrationModal.tsx
@@ -21,7 +21,7 @@ const RegistrationModal: React.FunctionComponent<RegistrationModalProps> = ({
     <Modal
       footer={null}
       width={isSuccess ? 622 : 1480}
-      className="border-0"
+      className="border-0 -p-10"
       open={open}
       onCancel={onClose}
       style={{ top: 0 }}
@@ -30,19 +30,23 @@ const RegistrationModal: React.FunctionComponent<RegistrationModalProps> = ({
       {isSuccess ? (
         <RegistrationCompleted />
       ) : (
-        <div className="p-[100px]">
+        <div className="md:p-[100px]">
           <Heading type="h1">
             Register to <span className="text-green-hover">attend</span>{" "}
           </Heading>
-          <div className="flex flex-row  items-start gap-[70px] self-stretch mt-[30px]">
+          <div className="flex flex-col md:flex-row  items-start md:gap-[70px] self-stretch mt-[30px]">
             <div className="basis-2/4">
               <AuthHeader />
-              <div className="mt-5">
+              <div className="mt-5 hidden md:block">
                 <PartnershipSection />
               </div>
             </div>
             <div className="basis-[75%]">
               <RegistrationForm />
+            </div>
+            <div className="block md:hidden">
+              <div className="h-[1px] w-full bg-gray-60 my-2.5" />
+              <PartnershipSection />
             </div>
           </div>
         </div>
