742e83448e891ac8b360ad5a7d530454d06a9f38
Chinweike Michael Chinonso <infinity_michael@yahoo.com>
2024-01-16 01:59:15 +0100

    feat: implemented admin registration dashboard ui

---
+++ src/app/app/registrations/components/RegistrationTab.tsx
@@ -0,0 +1,43 @@
+import { Tabs, TabsProps } from "antd";
+import React from "react";
+
+interface RegistrationTabProps {
+  /* eslint-disable-next-line */
+  onTabSelect?: (key: string) => void
+  defaultKey?: string
+}
+
+const RegistrationTab: React.FunctionComponent<RegistrationTabProps> = ({ onTabSelect, defaultKey }) => {
+  const onChange = (key: string) => {
+    if (onTabSelect) {
+      onTabSelect(key)
+    }
+
+  };
+
+  const items: TabsProps["items"] = [
+    {
+      key: "Submissions",
+      label: "Submissions",
+    },
+    {
+      key: "pDeclined Registrationsaid",
+      label: "Declined Registrations",
+    },
+  ];
+  return (
+    <Tabs
+      tabBarStyle={{
+        fontWeight: 600,
+        // paddingInline: 30,
+        // marginBottom: 40
+      }}
+      tabBarGutter={30}
+      defaultActiveKey={defaultKey}
+      items={items}
+      onChange={onChange}
+    />
+  );
+};
+
+export default RegistrationTab;
---
+++ src/app/app/registrations/hooks/index.ts
@@ -0,0 +1,1 @@
+export {default as useRegTableColumn } from './useRegTableColumn'
